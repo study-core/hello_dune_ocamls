3a031a323e11bde14bb85e3192758c696a5c8f5e
GavinXu520 <meiyan532484710@gmail.com>
2023-08-14 08:25:42 +0000

    add some functor grammar...
    Signed-off-by: GavinXu520 <meiyan532484710@gmail.com>

--- test/functor_test.ml
+++ test/functor_test.ml
@@ -14,26 +14,54 @@
     ############          ############
     ##################################
 
+
+
+
+
+
+######################################################################################################################################################
+
+
     【函数器】本质上是根据其他模块编写模块的一种方式
 
     【仿函数】是一个由【另一个模块】参数化的模块，
     就像【函数】是一个由【其他值】（参数）参数化的值一样
 
-    module 名称 (参数名称 : 签名表达式) = 模块化表达式
 
-    以下糖衣语法
+######################################################################################################################################################
 
-    module 名称 = functor (参数名称 : 签名表达式) -> 模块化表达式   
+
+
+
+
+
+
+    【module 名称 (参数名称 : 签名表达式) = 模块化表达式】
+
+
+    以下糖衣语法:
+
+    【module 名称 = functor (参数名称 : 签名表达式) -> 模块化表达式  】
+
+
+######################################################################################################################################################
 *)
 
 
-(* ----------------------------------------------------------------------------------------- *)
-(* -------------------------------- 【不】具备返回值的写法  -------------------------------- *)
+(* 
+######################################################################################################################################################
+【不】具备返回值的写法 
+######################################################################################################################################################
+*)
 
 (* 
     签名定义  (也是 命名模块类型 (模块类型定义))
     (查看 module_test.ml 文件对比)
+
     module type ELEMENT = sig type t val compare : t -> t -> int end
+
+
+    下面这个是 module 的类型签名 定义
 *)
 module type ELEMENT = sig
   type t
@@ -44,6 +72,7 @@ end
     functor
 
     语法： 
+    
     (* X 是将作为参数传递的模块， X_type 是它的签名 *)
     module F (X : X_type) = struct   
       ...
@@ -70,7 +99,11 @@ end
         val add : Element.t -> Element.t list -> Element.t list
         val elements : 'a -> 'a
       end
-定义 *)
+
+
+下面这个是   函子  MakeSet 的定义：
+
+*)
 module MakeSet (Element : ELEMENT) =
   struct
 
@@ -82,17 +115,17 @@ module MakeSet (Element : ELEMENT) =
     let mem x set = List.exists (fun y -> Element.compare x y = 0) set
 
     let rec add elt = function  (* 往 list 中添加新元素的 add 函数 *)
-    | [] -> [elt]
-    (* 从 list 中的 首元素 开始 比较 *)
-    | (x :: rest as s) ->
-        
-        match Element.compare elt x with
-        (* 如果 等于首元素， 则不做 add 操作 *)
-        | 0 -> s
-        (* 如果 不等于 首元素 且 < 0 则加到 list 尾部 *)
-        | r when r < 0 -> elt :: s
-        (* 否则，对比 list 下一个 元素 *)
-        | _ -> x :: (add elt rest)
+                            | [] -> [elt]
+                            (* 从 list 中的 首元素 开始 比较 *)
+                            | (x :: rest as s) ->
+
+                                match Element.compare elt x with
+                                (* 如果 等于首元素， 则不做 add 操作 *)
+                                | 0 -> s
+                                (* 如果 不等于 首元素 且 < 0 则加到 list 尾部 *)
+                                | r when r < 0 -> elt :: s
+                                (* 否则，对比 list 下一个 元素 *)
+                                | _ -> x :: (add elt rest)
 
     let rec elements s = s
   end;;
@@ -100,7 +133,8 @@ module MakeSet (Element : ELEMENT) =
 
 
 (* 
-    使用， 则
+    使用函子实例化一个  String 类型的 StringSet module：
+
     module StringSet :
       sig
         type elt = String.t
@@ -111,17 +145,27 @@ module MakeSet (Element : ELEMENT) =
         val elements : 'a -> 'a
       end 
 *)
-module StringSet = MakeSet(String);;
+
+module StringSet = MakeSet(struct
+    type t = int
+    let compare = compare
+  end);;
 
 
-(* ----------------------------------------------------------------------------------------- *)
-(* -----------------------------------  具备返回值的写法 ----------------------------------- *)
+
+(* 
+######################################################################################################################################################
+具备返回值的写法
+######################################################################################################################################################
+*)
+
 
 (* 
     module 名称 (参数名称 : 输入签名表达式)  : 签名表达式返回 = 模块表达式   
 
 *)
 module MakeSet (Element : ELEMENT) :
+
   (* 签名表达式返回 *)
   sig
     type elt = Element.t
@@ -130,7 +174,11 @@ module MakeSet (Element : ELEMENT) :
     val add : elt -> t -> t
     val elements : t -> elt list
   end
+
+
   =
+
+
   (* 模块化: 模块表达式  *)
   struct
     type elt = Element.t
@@ -155,7 +203,8 @@ module MakeSet (Element : ELEMENT) :
 
 
 (* 
-    使用，则
+    使用函子实例化一个  Int  类型的 IntSet module 并返回 (可以用变量接收该返回 module 的值)： 
+
     module IntSet :
       sig
         type elt = int
@@ -192,7 +241,10 @@ let s1 = add 1 (add 2 empty) and s2 = add 3 (add 4 empty);;
         val elements : t -> elt list
       end
 *)
-module StringSet = MakeSet(String);;
+module StringSet = MakeSet(struct
+    type t = String
+    let compare i j = 0
+  end);;
 
 (* 
     val s1 : StringSet.t = <abstr> (* 这是一个抽象的数据类型 *)
@@ -229,3 +281,136 @@ module type E1 =
       end
 *)
 module type E2 = E1 with type elt = int;;
+
+
+
+
+
+
+(* 
+********************************************************************
+示例    使用 Set 库的 Make() 函子 实例化一个 int 类型的 Set module
+********************************************************************
+*)
+
+(* 
+module 类型：
+
+module Int_set :
+  sig
+    type elt = int
+    type t
+    val empty : t
+    val is_empty : t -> bool
+    val mem : elt -> t -> bool
+    val add : elt -> t -> t
+    val singleton : elt -> t
+    val remove : elt -> t -> t
+    val union : t -> t -> t
+    val inter : t -> t -> t
+    val disjoint : t -> t -> bool
+    val diff : t -> t -> t
+    val compare : t -> t -> elt
+    val equal : t -> t -> bool
+    val subset : t -> t -> bool
+    val iter : (elt -> unit) -> t -> unit
+    val map : (elt -> elt) -> t -> t
+    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+    val for_all : (elt -> bool) -> t -> bool
+    val exists : (elt -> bool) -> t -> bool
+    val filter : (elt -> bool) -> t -> t
+    val filter_map : (elt -> elt option) -> t -> t
+    val partition : (elt -> bool) -> t -> t * t
+    val cardinal : t -> elt
+    val elements : t -> elt list
+    val min_elt : t -> elt
+    val min_elt_opt : t -> elt option
+    val max_elt : t -> elt
+    val max_elt_opt : t -> elt option
+    val choose : t -> elt
+    val choose_opt : t -> elt option
+    val split : elt -> t -> t * bool * t
+    val find : elt -> t -> elt
+    val find_opt : elt -> t -> elt option
+    val find_first : (elt -> bool) -> t -> elt
+    val find_first_opt : (elt -> bool) -> t -> elt option
+    val find_last : (elt -> bool) -> t -> elt
+    val find_last_opt : (elt -> bool) -> t -> elt option
+    val of_list : elt list -> t
+    val to_seq_from : elt -> t -> elt Seq.t
+    val to_seq : t -> elt Seq.t
+    val to_rev_seq : t -> elt Seq.t
+    val add_seq : elt Seq.t -> t -> t
+    val of_seq : elt Seq.t -> t
+  end
+*)
+module Int_set =
+  Set.Make (struct
+              type t = int
+              let compare = compare
+            end);;
+
+
+
+
+(* 
+********************************************************************
+示例    使用 Set 库的 Make() 函子 实例化一个 String 类型的 Set module
+********************************************************************
+*)
+
+(* 
+module String_set :
+  sig
+    type elt = string
+    type t = Set.Make(String).t
+    val empty : t
+    val is_empty : t -> bool
+    val mem : elt -> t -> bool
+    val add : elt -> t -> t
+    val singleton : elt -> t
+    val remove : elt -> t -> t
+    val union : t -> t -> t
+    val inter : t -> t -> t
+    val disjoint : t -> t -> bool
+    val diff : t -> t -> t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val subset : t -> t -> bool
+    val iter : (elt -> unit) -> t -> unit
+    val map : (elt -> elt) -> t -> t
+    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+    val for_all : (elt -> bool) -> t -> bool
+    val exists : (elt -> bool) -> t -> bool
+    val filter : (elt -> bool) -> t -> t
+    val filter_map : (elt -> elt option) -> t -> t
+    val partition : (elt -> bool) -> t -> t * t
+    val cardinal : t -> int
+    val elements : t -> elt list
+    val min_elt : t -> elt
+    val min_elt_opt : t -> elt option
+    val max_elt : t -> elt
+    val max_elt_opt : t -> elt option
+    val choose : t -> elt
+    val choose_opt : t -> elt option
+    val split : elt -> t -> t * bool * t
+    val find : elt -> t -> elt
+    val find_opt : elt -> t -> elt option
+    val find_first : (elt -> bool) -> t -> elt
+    val find_first_opt : (elt -> bool) -> t -> elt option
+    val find_last : (elt -> bool) -> t -> elt
+    val find_last_opt : (elt -> bool) -> t -> elt option
+    val of_list : elt list -> t
+    val to_seq_from : elt -> t -> elt Seq.t
+    val to_seq : t -> elt Seq.t
+    val to_rev_seq : t -> elt Seq.t
+    val add_seq : elt Seq.t -> t -> t
+    val of_seq : elt Seq.t -> t
+  end    
+*)
+module String_set =
+  Set.Make (struct
+              type t = String
+              let compare = compare
+            end);;
+           
