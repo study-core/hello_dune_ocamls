10f0dad6ebea54f2b08c0eff03b1071e067cf6e7
GavinXu520 <meiyan532484710@gmail.com>
2024-09-02 02:45:10 +0000

    add comment
    Signed-off-by: GavinXu520 <meiyan532484710@gmail.com>

---
+++ test/json_test4.ml
@@ -0,0 +1,28 @@
+#use "topfind";;
+#require "ppx_deriving_yojson";;
+
+(* open Yojson;; *)
+
+(* Define an OCaml type *)
+type person = {
+  name : string;
+  age : int;
+  slave: string;
+  } 
+[@@deriving to_yojson]
+
+(* Example person *)
+let my_person = { name = "Gavin"; age = 36; slave = "Kally" }
+(* let xx = person_to_yojson my_person *)
+
+(* Convert person to JSON and store in a file *)
+let store_person_as_json_file filename person =
+  let json_str = Yojson.Safe.to_string (person_to_yojson person) in
+
+  (* 流操作 *)
+  let oc = open_out filename in
+  output_string oc json_str;
+  close_out oc
+
+(* Usage *)
+let () = store_person_as_json_file "person.json" my_person
