7c7276c563dd01b73972eb85cc58a0762f13ba6d
GavinXu520 <meiyan532484710@gmail.com>
2024-09-10 01:39:10 +0000

    fixed
    Signed-off-by: GavinXu520 <meiyan532484710@gmail.com>

--- test/json_test.ml
+++ test/json_test.ml
@@ -18,7 +18,9 @@ open Yojson;;
 let json_string = {|
   {"number" : 42,
    "string" : "yes",
-   "list": ["for", "sure", 42]}|}
+   "list": ["for", "sure", 42]
+  }
+|}
 (* val json_string : string *)
 
 let json = Yojson.Safe.from_string json_string
